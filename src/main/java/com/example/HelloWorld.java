850a62f06f76968c792eb82f4185ec554dfaed0f
gzq <694326243@qq.com>
2016-10-24 14:28:13 +0800

    提交helloworld

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package main.java.com.example;
+
+/**
+ * Created by admin on 2016/10/24.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
